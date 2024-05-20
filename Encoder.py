c4faff8cd3c6f284676c8957741f1665342b4dd1
Olasubomi O. Badiru <badirusubomi@icloud.com>
2024-05-20 05:25:52 -0600

    Databases implemented, UI changes

--- Encoder.py
+++ Encoder.py
@@ -3,6 +3,43 @@ import dlib
 import face_recognition
 import pickle
 import os
+import firebase_admin
+from firebase_admin import  storage, credentials
+
+
+
+
+class EncoderDB():
+    ''' The only mehod you need from this class is uploadFile() '''
+    def __init__(self, name = "Test Encoder DB"):
+        self._cred = credentials.Certificate("serviceAccountKey.json")
+        firebase_admin.initialize_app(self._cred, {
+            'databaseURL': "https://spring-capstone-c5472-default-rtdb.firebaseio.com/",
+            'storageBucket': 'spring-capstone-c5472.appspot.com'
+        })
+        self._name = name
+        self._bucket = storage.bucket()
+        self._blob = None
+    
+    def __str__(self):
+        return self._name
+    
+    def uploadFile(self, fName):
+        ''' 
+        no need to specify path before calling, this 
+        function finds the path using global variable - folderPath
+        '''
+        fileName = self.getFname(fName)
+        self._blob = self._bucket.blob(fileName)
+        try:    
+            self._blob.upload_from_filename(fileName)
+            print("Upload succesful")
+        except Exception as e:
+            print("Upload unsuccesful: ",e)
+
+    def getFname(self, path):
+        return f"{folderPath}/{path}" #os.path.join(folderPath, path)
+    
 
 # Importing student images
 folderPath = "Images"
@@ -35,3 +72,11 @@ pickle.dump(encodeListKnownIds, file)
 file.close
 print("File Saved")
 
+    
+
+
+if __name__ == '__main__':
+
+    # Usage
+    testEncodeDB = EncoderDB()
+    status = testEncodeDB.uploadFile("3080126.png")
